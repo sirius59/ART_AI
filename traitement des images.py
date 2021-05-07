2c04d36d92963d53d446b5022558c6416e35b8f1
sirius59 <82665335+sirius59@users.noreply.github.com>
2021-05-07 21:49:26 +0200

    Add files via upload

---
+++ traitement des images.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import pandas as pd
+
+artists=pd.read_csv('C:\\Users\\cleme\\Downloads\\dataset\\artists.csv')
+
+##name of artists
+artists['name'][19]='Albrecht_Durer' #fix ASCII problems
+tmp=artists['name']
+name=[tmp[i].replace(' ','_') for i in range(len(tmp))]#replace space by underscore, more convinent to import images
+
+##genre of artists
+tmp=artists['genre']
+genre=[tmp[i].replace(',','/') for i in range(len(tmp))]#replace coma by slash, avoid mistakes in list
+
+##number of paintings by artists
+nb_paints=artists['paintings']
+
+
+for i in range(len(name)):
+  for j in range(nb_paints[i]):
+    img=Image.open(f'C:\\Users\\cleme\\Downloads\\dataset\\images\\{name[i]}\\{name[i]}_{j+1}.jpg')
+    new=img.resize((224,224),resample=3)
+    new.save('C:\\Users\\cleme\\Downloads\\dataset\\resized\\'+str(name[i])+'_'+str(j+1)+'.jpg')
